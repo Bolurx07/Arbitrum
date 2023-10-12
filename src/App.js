3be5b72f8cfbd00c295d011fc87efe00f5d70dd8
Israel <iaboluwatife@gmail.com>
2023-10-12 14:16:20 +0100

    New update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Index from './Home/Index';
+
+function App() {
+  return (
+    <div>
+      <Index />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home/Index.jsx
@@ -0,0 +1,44 @@
+import './Index.css';
+import { FaTwitter } from 'react-icons/fa';
+import mainlogo from '../Images/logo-main.png';
+import snap from '../Images/snap.png';
+import arbinauts from '../Images/three-arbinauts.webp';
+import wallet from '../Images/wallet-button.png';
+import moon from '../Images/moon.webp';
+const Index = () => {
+  return (
+    <div className='homePage'>
+          <img src={moon} alt="moon" className='moon'/>
+
+        <nav className='myNav'>
+            <img src={mainlogo} alt="logo" />
+            <div className='navRight'>
+              <div><img src={wallet} alt="wallet button"  /></div>
+              <div><img src={snap} alt="logo" /></div>
+              <div><FaTwitter size={25}/></div>
+            </div>
+        </nav>
+        <div className='midpage'>
+          <p>Please connect your wallet to use the bridge.</p>
+          <img src={arbinauts} alt="arbinauts" srcset="" />
+        </div>
+        <div className='footer'>
+          <h4>The most decentralized L2</h4>
+          <p className='learn'>Learn More.</p>
+          <div className='footlinks'>
+            <div>
+              <p>Documentation</p>
+              <p>Careers</p>
+              <p>Blog</p>
+            </div>
+            <div>
+              <p>Recent Press</p>
+              <p>ToS</p>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Index
